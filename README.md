ded56a7f45c276fe4a3678f765523d7ce881422b
Rylee George <ryleegeorge1@gmail.com>
2023-10-01 23:40:33 +1100

    Init

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # fastify-cf-turnstile
+A [Cloudflare Turnstile](https://developers.cloudflare.com/turnstile/) plugin for fastify
+## Example
+
+```javascript
+const cfTurnstile = require('fastify-cf-turnstile')
+fastify.register(cfTurnstile,{
+    sitekey:"your_sitekey",
+    privatekey:"your_privatekey",
+})
+```
+
+Using in a route
+
+```javascript
+  fastify.post('/login', {
+    preValidation: fastify.cfTurnstile,
+    schema: {
+      summary: 'User login',
+      body: {
+        type: 'object',
+        properties: {
+          email: {
+            anyOf: [
+              { type: 'string' },
+              { type: 'object' }
+            ]
+          },
+          password: {
+            anyOf: [
+              { type: 'string' },
+              { type: 'object' }
+            ]
+          }
+        },
+        required: ['email', 'password']
+      }
+    }
+  },
+  async function (req, reply) {
+    // Login logic
+  })
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+const fastifyPlugin = require('fastify-plugin')
+
+async function fastifyCloudFlareTurnstile(fastify, options, done) {
+  if (!Object.prototype.hasOwnProperty.call(options, 'sitekey')) {
+    throw new Error('sitekey must be set as a string in the options object.')
+  }
+  if (typeof options.sitekey !== 'string') {
+    throw new Error('sitekey must be a string in the options object.')
+  }
+  if (!Object.prototype.hasOwnProperty.call(options, 'privatekey')) {
+    throw new Error('privatekey must be set as a string in the options object.')
+  }
+  if (typeof options.privatekey !== 'string') {
+    throw new Error('privatekey must be a string in the options object.')
+  }
+  fastify.decorate('cfTurnstile', cfTurnstile)
+
+  async function cfTurnstile(req, reply, next) {
+    const token = req.body['cf-turnstile-response']?.value || req.body['cf-turnstile-response']
+    await verify(req, token, options.privatekey, next)
+  }
+
+  async function verify(req, token, privatekey, next) {
+    const ip = req.headers['cf-connecting-ip']
+
+    const formData = new FormData()
+    formData.append('secret', privatekey)
+    formData.append('response', token)
+    formData.append('remoteip', ip)
+
+    const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
+    const result = await fetch(url, {
+      body: formData,
+      method: 'POST'
+    })
+    const outcome = await result.json()
+    if (!outcome.success) {
+      const err = new Error('Invalid Captcha')
+      err.status = 400
+      throw err
+    }
+    return next()
+  }
+  done()
+}
+
+module.exports = fastifyPlugin(fastifyCloudFlareTurnstile)
